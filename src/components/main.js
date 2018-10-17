89894fec1bdd42697fb5793f7fe8861a884d17c8
peterwateber <peterwateber@gmail.com>
2018-10-17 19:53:37 +0800

    Default

---
+++ src/components/main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>To do list:</h1>
+            </div>
+        );
+    }
+}
